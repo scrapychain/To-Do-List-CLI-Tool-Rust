05edf134f48489196dea29d60ea1539b7df705bc
scrapychain <mindquaker20@gmail.com>
2026-02-04 22:36:15 -0500

    added functionality to add, remove, list items into the todo list and then quit.

---
+++ src/main.rs
@@ -0,0 +1,38 @@
+use std::io::{self, Read};
+fn main() {
+    let mut items: Vec<String> = Vec::new();
+
+    loop {
+        println!("Todo CLI - Enter command (add/list/remove/quit):");
+        let mut input = String::new();
+
+        io::stdin().read_line(&mut input).unwrap();
+        let command = input.trim();
+
+        match command {
+            "add" => {
+                println!("Enter an item");
+                let mut item = String::new();
+                io::stdin().read_line(&mut item).unwrap();
+                items.push(item);
+                println!("Added!");
+            }
+            "remove" => {
+                println!("Enter item number that you want to delete (0 to n-1)");
+                let mut index = String::new();
+                io::stdin().read_line(&mut index).unwrap();
+
+                let index_number: usize =
+                    index.trim().parse().expect("Please enter a valid number");
+                items.remove(index_number);
+            }
+            "list" => {
+                for (i, item) in items.iter().enumerate() {
+                    println!("{} : {}", i, item);
+                }
+            }
+            "quit" => break,
+            _ => println!("Unknown Command"),
+        }
+    }
+}
